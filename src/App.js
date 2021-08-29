74b89da927a6bd34799d739fbb0703390a0c29a9
Maksim Tsopa <maks98242@gmail.com>
2021-08-29 07:12:35 +0300

    final ver

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import { Switch, Redirect } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, Suspense, lazy } from 'react';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+import Container from 'components/Container/Container';
+import AppBar from 'components/AppBar/AppBar';
+import PrivateRoute from 'components/PrivateRoute';
+import { fetchCurrentUser } from 'redux/auth/auth-operations';
+import PublicRoute from 'components/PublicRoute';
+import { getFetchingCurrent } from 'redux/auth/auth-selectors';
+const HomeView = lazy(() => import('views/HomeView'));
+const RegisterView = lazy(() => import('views/RegisterView'));
+const LoginView = lazy(() => import('views/LoginView'));
+const ContactsView = lazy(() => import('views/ContactsView'));
+
+export default function App() {
+  const dispatch = useDispatch();
+  const isFetchingCurrentUser = useSelector(getFetchingCurrent);
+  useEffect(() => dispatch(fetchCurrentUser()), [dispatch]);
+  return (
+    <Container>
+      {isFetchingCurrentUser ? (
+        <h2>Loading</h2>
+      ) : (
+        <>
+          <AppBar />
+
+          <Switch>
+            <Suspense fallback={<p>Loading...</p>}>
+              <PublicRoute exact path="/">
+                <HomeView />
+              </PublicRoute>
+              <PublicRoute exact path="/register" restricted>
+                <RegisterView />
+              </PublicRoute>
+              <PublicRoute
+                exact
+                path="/login"
+                redirectTo="/contacts"
+                restricted
+              >
+                <LoginView />
+              </PublicRoute>
+              <PrivateRoute path="/contacts">
+                <ContactsView />
+              </PrivateRoute>
+              <Redirect to="/" />
+            </Suspense>
+          </Switch>
+          <ToastContainer position="bottom-left" autoClose={3000} />
+        </>
+      )}
+    </Container>
+  );
+}
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,43 @@
+import PropTypes from 'prop-types';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getFilteredContacts } from 'redux/contacts/contacts-selectors';
+import {
+  fetchContacts,
+  deleteContacts,
+} from 'redux/contacts/contacts-operations';
+import s from './ContactList.module.css';
+
+export default function ContactList() {
+
+  const contacts = useSelector(getFilteredContacts);
+  const dispatch = useDispatch();
+  const onDeleteClick = id => dispatch(deleteContacts(id));
+  useEffect(() => dispatch(fetchContacts()), [dispatch]);
+
+  return (
+    <ul className={s.contactList}>
+      {contacts.map(({ id, name, number }) => {
+        return (
+          <li key={id} className={s.contact}>
+            <p>
+              {name}: {number}
+            </p>
+            <button
+              className={s.buttonDelete}
+              type="button"
+              onClick={() => onDeleteClick(id)}
+            >
+              Delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+ContactList.propTypes = {
+  contacts: PropTypes.array,
+  onDeleteClick: PropTypes.func,
+};
